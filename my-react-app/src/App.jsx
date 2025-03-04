d3b141ac40c8e17b7a4ac452aaca8d6662024ed0
Nagraj <nagrajhm2002@gmail.com>
2025-03-04 11:18:54 +0530

    calculator

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,16 +4,18 @@ import Form from "./components/Form";
 import AddTwoNumber from "./components/AddTwoNumbers";
 import SubTwoNumbers from "./components/SubTwoNumbers";
 import MulTwoNumbers from "./components/MulTwoNumbers";
+import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <div>
       {/*<Increment />*/}
       {/*<IncDec />*/}
-      <Form />
+      {/*<Form />
       <AddTwoNumber />
       <SubTwoNumbers />
-      <MulTwoNumbers/>
+      <MulTwoNumbers/>*/}
+      <Calculator/>
     </div>
   );
 };
