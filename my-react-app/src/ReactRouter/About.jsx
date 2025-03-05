220b4cc91274dea5b7db1282872f4796f0a40eaa
Nagraj <nagrajhm2002@gmail.com>
2025-03-05 15:48:29 +0530

    React Router commit

---
+++ my-react-app/src/ReactRouter/About.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+function About(){
+    return (<div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsam excepturi itaque earum optio facere tempora ab vero esse voluptatem, deleniti ad at necessitatibus incidunt ut rem voluptas expedita eligendi.</p>
+    </div>)
+};
+
+
+export default About;
---
+++ my-react-app/src/ReactRouter/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="order-list-item">
+            <nav>
+                <ul className="order-list">
+                    <li>List 1</li>
+                    <li>List 2</li>
+                    <li>List 3</li>
+                    <li>List 4</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Home;
